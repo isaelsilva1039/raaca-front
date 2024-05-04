29ec53f61f038b55a1531f4d02bc532ba8d44cf0
ezequiel8841 <ezequiel_cardoso@live.com>
2024-05-04 15:07:21 -0300

    melhorias na modal do perfil, adcao de informacao do emal, botao de gerenciar conta

--- src/ui/components/modal.tsx
+++ src/ui/components/modal.tsx
@@ -1,6 +1,6 @@
-// Modal.jsx
 import React from 'react';
 import './modal.css'; 
+import { AiOutlineClose, AiOutlineSetting } from 'react-icons/ai'; // Importando ícones
 import { useCliente } from '@/core/helpes/UserContext';
 
 interface ModalProps {
@@ -10,14 +10,12 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ onClose, handleLogout, profilePhoto }) => {
-
-    const { user, token, logout , avatar_user } = useCliente();
+  const { user, avatar_user } = useCliente();
 
   const handleLogoutClick = () => {
     handleLogout();
     onClose();
   };
-
 
   const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
     if (event.target === event.currentTarget) {
@@ -25,21 +23,26 @@ const Modal: React.FC<ModalProps> = ({ onClose, handleLogout, profilePhoto }) =>
     }
   };
 
-  // Remove as aspas da string se estiverem presentes
-const cleanedAvatar = avatar_user.replace(/^"|"$/g, '');
-
+  const cleanedAvatar = avatar_user.replace(/^"|"$/g, '');
 
   return (
     <div className="modal-backdrop" onClick={handleBackdropClick}>
-    
       <div className="modal-content">
+        <button className="modal-close-btn" onClick={onClose}><AiOutlineClose /></button> 
+        <text className="emailModal" style={{color:'black'}}> {user.email} </text>
         <img src={cleanedAvatar ?  cleanedAvatar : profilePhoto } alt="Profile" className="modal-profile-image" />
         <h2 className="modal-title">
-          <text style={{color:'black'}}> Olá, {user?.name} </text></h2>
-        <p  style={{color:'black', fontWeight:300}}>
-          Aqui ainda vamos definir oque vamos colocar
+          <text style={{color:'black'}}> Olá, {user?.name} </text>
+        </h2>
+        <button className="modal-manage-btn">
+          <AiOutlineSetting style={{ marginRight: '5px' }} /> Gerenciar sua Conta RACCA
+        </button>
+        <p style={{color:'black', fontWeight:300}}>
+          Aqui ainda vamos definir o que vamos colocar
         </p>
-        <a className="modal-logout-btn" onClick={handleLogoutClick}>Sair</a>
+        <div className="modal-buttons">
+          <a className="modal-logout-btn" onClick={handleLogoutClick}>Sair</a>
+        </div>
       </div>
     </div>
   );
