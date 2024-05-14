ae8cc6c7db2dad6d2ea12c66e9c81a9c55dd71c7
ezequiel8841 <ezequiel_cardoso@live.com>
2024-05-13 21:46:11 -0300

    correcao configuração

--- next.config.js
+++ next.config.js
@@ -2,10 +2,9 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['127.0.0.1'], // Adicione todos os domínios dos quais você carrega imagens
-    },
-  }
-  
-  module.exports = nextConfig;
-  
+  images: {
+    domains: ['127.0.0.1', 'racca.store'], 
+  },
+}
+
+module.exports = nextConfig;
