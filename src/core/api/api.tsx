fbd9058682f3cf3c07e7d9cadfed6d49bc5ed784
Isael <isaelsiva1039@gmail.com>
2024-06-30 18:35:57 -0300

    ajustes

--- src/core/api/api.tsx
+++ src/core/api/api.tsx
@@ -1,5 +1,5 @@
-//  export const  API = 'https://racca.store'
-export const  API = 'http://127.0.0.1:8000';
+ export const  API = 'https://racca.store'
+// export const  API = 'http://127.0.0.1:8000';
  
 
 
